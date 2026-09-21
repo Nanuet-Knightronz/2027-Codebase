48fcb391492bf5ff68170f4ef9b3ae4d0fef09f5
LtBrain <brian.j.guan@outlook.com>
2026-09-21 01:19:32 -0400

    Add 2026 Field Constants
    - Add FieldConstants.java from 6328
    - Create /util directory to house future files
    - Add AllianceFlipUtil.java to handle alliance in conjunction with FieldConstants.java
    - Add Bounds.java as a dependency
    - Add Lombok to build.gradle

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -48,4 +48,10 @@ public final class Constants
     public static final double RIGHT_X_DEADBAND = 0.1;
     public static final double TURN_CONSTANT    = 6;
   }
+
+  public static boolean disableHAL = false;
+
+  public static void disableHAL() {
+    disableHAL = true;
+  }
 }
